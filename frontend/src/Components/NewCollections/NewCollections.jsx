52477ad2733041b7a7ec8655e8359d6dbce6b472
sharonkumar1000 <163455102+sharonkumar1000@users.noreply.github.com>
2024-07-07 18:13:39 +0530

    Update NewCollections.jsx

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './NewCollections.css'
 import Item from '../Item/Item'
 
+
 const NewCollections = () => {
   const [new_collection,setNew_collection] = useState([])
   const url = 'https://ecommerce-backend-zsxd.onrender.com'
